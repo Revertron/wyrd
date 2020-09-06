f87fd7b451ee8b405b3c4664a700db44d8e8667f
Revertron <github@zbin.eu>
2020-09-06 15:48:20 +0200

    Autocommit changed zone.

--- db/domains.sql
+++ db/domains.sql
@@ -102,6 +102,7 @@ INSERT INTO `domains` VALUES ('tdemin.ygg','228:e140:b0bc:d538:1a7a:5d1e:6a29:15
 INSERT INTO `domains` VALUES ('telegram-proxy.ygg','200:149b::b722:fe09:d31:84a4',NULL,0,1572364588,0,NULL,'telegram-proxy	300	IN	AAAA	200:2962::64cd:7a09:c268:a954\npx.telegram-proxy	120	IN	AAAA	200:2962::64cd:7a09:c268:a954\npx.telegram-proxy	120	IN	AAAA	201:2522::5736:9a6:7dd1:7c95');
 INSERT INTO `domains` VALUES ('tepolli.ygg','201:8538:fde:5d90:2226:c723:5d60:a034',NULL,0,1589280559,0,NULL,'tepolli.ygg.	3600	IN	AAAA	301:8538:fde:5d90::6');
 INSERT INTO `domains` VALUES ('test.ygg','200:2962::64cd:7a09:c268:a954',NULL,0,1572364588,0,NULL,'test	600	IN	AAAA	200:2962::64cd:7a09:c268:a954\n*.test	300	IN	AAAA	200:2962::64cd:7a09:c268:a954\nddd.test	300	IN	AAAA	301:2923::53');
+INSERT INTO `domains` VALUES ('theoph.ygg','200:75fb:a075:b9b7:d1b8:8f35:83b5:c0fe',NULL,0,1599400100,0,NULL,'theoph.ygg.	3600	IN	AAAA	200:75fb:a075:b9b7:d1b8:8f35:83b5:c0fe');
 INSERT INTO `domains` VALUES ('tor-bridge.ygg','223:f05:3dee:526c:d4ec:e65c:457b:7fcf',NULL,1,0,0,NULL,'tor-bridge	IN	AAAA	223:f05:3dee:526c:d4ec:e65c:457b:7fcf');
 INSERT INTO `domains` VALUES ('torrent-tracker.ygg','201:4541:2f84:1188:59ab:e8dd:48a8:d40c',NULL,0,0,0,NULL,'torrent-tracker	IN	AAAA	301:4541:2f84:1188:216:3eff:fe34:ec44');
 INSERT INTO `domains` VALUES ('transmission.ygg','200:a0c1:5204:9f99:807d:d4cd:6698:98a3',NULL,1,0,0,NULL,'transmission	IN	AAAA	200:a0c1:5204:9f99:807d:d4cd:6698:98a3');
